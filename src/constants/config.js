a0a5c7f4e8c490726bd33a79bef94ae88f2b0673
kylekmcleod <kylekmcleod1@gmail.com>
2026-07-04 20:20:30 -0700

    folder setup boilerplate

---
+++ src/constants/config.js
@@ -0,0 +1,12 @@
+export const DEALERSHIP = {
+  name: 'August Motor Cars',
+  location: 'Kelowna, BC',
+  agentName: 'Jessica',
+}
+
+export const WIDGET_VIEWS = {
+  CLOSED: 'closed',
+  LANDING: 'landing',
+  CHAT: 'chat',
+  VOICE: 'voice',
+}
---
+++ src/constants/links.js
@@ -0,0 +1,12 @@
+export const LINKS = {
+  augustGarage: {
+    label: 'August Garage',
+    description: 'Service department',
+    url: 'https://augustmotorcars.com/service',
+  },
+  augustCustoms: {
+    label: 'August Customs',
+    description: 'Custom shop',
+    url: 'https://augustmotorcars.com/customs',
+  },
+}
